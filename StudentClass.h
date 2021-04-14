b7cc696ce711de1f1a44c7dc5aff58653b6243a4
Rusal84 <rusal@bk.ru>
2021-04-15 01:34:33 +0300

    add studentClass

--- StudentClass.h
+++ StudentClass.h
@@ -1,4 +1,18 @@
-
+#include <string>
+#include "BirthDateClass.h"
+#include "ExamsResultsClass.h"
+using namespace std;
 class StudentClass
 {
+	string SurName;
+	string Name;
+	string MiddleName;
+	string Faculty;
+	string Department;
+	string Group;
+	string —redit—ardNumber;
+	string Sex;
+	BirthDateClass BirthDate;
+	ExamsResultsClass ExamsResults;
+
 };
