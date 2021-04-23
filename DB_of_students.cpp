4cad98428920bfbd12ea66515b2b47e5fdd5d4be
Rusal84 <rusal@bk.ru>
2021-04-23 19:41:35 +0300

    fix

--- DB_of_students.cpp
+++ DB_of_students.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <iostream>
 #include "StringBuilderClass.h"
+#include "FileManagerClass.h"
 
 int main()
 {
@@ -10,12 +11,15 @@ int main()
     StringBuilderClass sb = StringBuilderClass();
     cout << endl;
     string s1 = sb.setParam("surName", "Ярошенко");
-    string s2 = sb.setParam("startYear", 2020);
-    cout << endl << s1 << "  " << sb.getType(s1) << endl;
-    cout << endl << s2 << "  " << sb.getType(s2) << endl;
+    //string s2 = sb.setParam("startYear", 2020);
+    //cout << endl << s1 << "  " << sb.getType(s1) << endl;
+    //cout << endl << s2 << "  " << sb.getType(s2) << endl;
 
-    sb.getName(s2);
-    sb.getValueInt(s2);
+    //sb.getName(s2);
+    //sb.getValueInt(s2);
+
+    FileManagerClass fb = FileManagerClass("d:\\DB.txt");
+    fb.AddRusakovRecord();
 
 }
 
